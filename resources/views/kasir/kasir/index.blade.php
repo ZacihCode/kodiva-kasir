65e91fca39e044b67d3a1821e32e4b4f11b68037
DhiasCoder <diashabibi8@gmail.com>
2025-08-18 16:30:06 +0700

    Remove Alpine.js script inclusion from the cashier index view

--- resources/views/kasir/kasir/index.blade.php
+++ resources/views/kasir/kasir/index.blade.php
@@ -6,7 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Wisata Sendang Plesungan - Kasir</title>
     <script src="https://cdn.tailwindcss.com"></script>
-    <script src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js" defer></script>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet">
     <style>
         @import url('https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap');
